5ec97a081bb74b6739ef3f06d022a1b3fccf1b0f
Hadeel Obaid <hadeelobaid71@gmail.com>
2023-09-28 15:27:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+let hadeel =[]
+const array = ["Guadalupe", "Ollie", "Aki"];
+const writeCards= function(array, event) {
+    for (let i = 0; i < array.length; i++) {
+      hadeel.push (`Thank you, ${array[i]}, for the wonderful surprise gift!`);  
+
+    }
+  return hadeel;     
+}
+
+
+/*let hello
+let countDown= function(hello) {
+    for (let i = hello; i >= 0; i--) {
+    console.log(hello--)
+      }
+}*/
+
+
+let countDown = function(down) {
+    while (down >= 0) {
+      console.log(down--);
+    }
+    }
